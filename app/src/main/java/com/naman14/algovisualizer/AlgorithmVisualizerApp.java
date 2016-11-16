8809ab7edab7ad9b425ad016759dce3ad5b5ff09
naman14 <namandwivedi14@gmail.com>
2016-11-16 17:31:49 +0530

    add crashlytics

---
+++ app/src/main/java/com/naman14/algovisualizer/AlgorithmVisualizerApp.java
@@ -0,0 +1,24 @@
+package com.naman14.algovisualizer;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by naman on 16/11/16.
+ */
+
+public class AlgorithmVisualizerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+        Fabric.with(this, crashlyticsKit);
+    }
+}
